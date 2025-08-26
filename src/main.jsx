688db71f1d0dfa61ecb2fd7ced317821d7806534
Johan Taborda <johan.david.taborda@gmail.com>
2025-08-25 19:04:50 -0500

    Directories are added for workspaces.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'; 
+
+import "./resources/styles/stylesGeneral.css"
+
+//Hacemos uso de Boostrap 5
+import 'bootstrap/dist/css/bootstrap.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import App from './modules/layouts/App/App';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 )
---
+++ src/service/axiosConfig.js
@@ -0,0 +1,1 @@
+//Archivo para manejar AXIOS para comunicarnos con el backend.
