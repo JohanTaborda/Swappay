d4dacc3f216cad1f5b5aa225eff49e9c3c3a6220
Johan Taborda <johan.david.taborda@gmail.com>
2025-09-30 23:48:42 -0500

    Se refactoriza ProtectedRoutes y MainData: mejora en la validación del usuario mediante el token.

--- src/App/routes/ProtectedRoutes.jsx
+++ src/App/routes/ProtectedRoutes.jsx
@@ -2,21 +2,46 @@ import { Navigate } from "react-router-dom";
 import { useUserStore } from "../stores/Store";
 import { useEffect, useState } from "react";
 
+const protectedRouters = ({ children }) => {
+    const { initializeUser, isVerified } = useUserStore(); // Funcion que valida el token.
+    const [loading, setLoading] = useState(true); // Estado para mostrar una carga mientras los datos se traen del back.
 
+    useEffect(() => {
 
-const protectedRouters = ({children}) => {
+        let isMounted = true; //Bandera para asegurarnos de no modificar el estado si el componente ya se desmontó
 
-    const {initializeUser, isVerified} = useUserStore(); //Función que valida el token.
-    const [loading, setLoading] = useState(true); //Estado para mostrar una carga mientras los datos se traen del back.
-    useEffect (() => {
-        const validateUser = async() => { //Valida el token y actualiza el estado.
-            await initializeUser()
-            setLoading(false);
+        //withLoader, es una propiedad que se recibe mediante props, en caso de no recibir un valor, el por defecto será 'false'.
+        //Si es verdadero, se activa la pantalla de carga
+        const validateUser = async (withLoader = false) => { //Función para la validación del usuario
+            if (withLoader && isMounted) {
+                setLoading(true);
+            }
+
+            try {
+                await initializeUser(); //Llamamos esta función del store para verificar el token con el backend.
+            } finally {
+                if (withLoader && isMounted) { //Si ambos valores son verdaderos, el componente sigue montado.
+                    setLoading(false); //Si esta montado, se quita el loading.
+                }
+            }
         };
-        validateUser();
-    },[initializeUser]);
 
-    return loading ? <div>Cargando...</div> : isVerified ? children:<Navigate to={"/ingresar"} replace/>;
-}
+        validateUser(true); //Llamamos la función al inicio para mostrar el 'loading' ya que el componente inicialmentes esta montado.
+
+        const intervalId = setInterval(() => {
+            validateUser(false);
+        }, 60 * 60 * 1000); // Cada 1 hora se valida el usuario otra vez. Pero sin loader para no molestar con pantallas de carga.
+
+        return () => { //Cuando el componente se desmonta.
+            isMounted = false; //Se cambia la bandera para que no intente actualizar el estado
+            clearInterval(intervalId); //Limpiamos el setInterval para evitar fugas en la memoria
+        };
+
+    }, [initializeUser]); //Cada vez que esta dependencia cambie, el useEffect se ejecuta.
+
+
+    //Si esta cargando el componente muestra el div, si el usuario es verificado muestra el componente protegido, si no esta verificado, redirige a '/ingresar'.
+    return loading ? <div>Cargando...</div> : isVerified ? children : <Navigate to={"/ingresar"} replace />;
+};
 
 export default protectedRouters;
