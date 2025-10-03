731948e7d0e0c366efba60ce8632380caa2f400c
AsleyT <torrespalacioas@gmail.com>
2025-10-03 17:06:10 -0500

    Se organizar el modo responsivo del panel de bienvenida y se agregó un difuminado sutil.

--- src/modules/layouts/main/MainPanel/components/MainData/MainData.jsx
+++ src/modules/layouts/main/MainPanel/components/MainData/MainData.jsx
@@ -20,15 +20,15 @@ const MainData = () =>{
         return () => clearInterval(interval) 
     },[initializeUser])
 
-    const Carousel = [
+    const Carousel = [ //Arreglo que contiene los datos para las tarjetas del carrusel
         {name: "Intercambios", img: Change, descr: "Intercambia lo que ya no usas por lo que realmente necesitas. Conecta con otros usuarios de forma práctica y segura, generando oportunidades de ahorro.", ruta: "/intercambios"},
         {name: "Ofertas", img: Discount, descr: "Accede a descuentos únicos en productos seleccionados, pero solo podrás adquirirlos utilizando tus Swappcoins.", ruta: "/ofertas"}
     ]
 
     return(
-        <div>
+        <div className="mainData">   
             <section className="mainDataHero">
-                <div className="mainDataContainerHero">
+                <div className="mainDataContainerHero">  {/*Crea mensaje de bienvenida personalizado*/}
                     <h3>Bienvenid@ {username}</h3>
                     <p>En este espacio podrás intercambiar, descubrir promociones y aprovechar al máximo tus Swapcoins.</p>
                 </div>
@@ -37,13 +37,14 @@ const MainData = () =>{
                 </div>
             </section>
                 
-            <section className="mainDataCarruselContainer">
-                {Carousel.map((value, index) =>(
+            {/*Crea una tarjeta por cada elemento del array Carousel con navegacion*/}
+            <section className="mainDataCarruselContainer"> 
+                {Carousel.map((value, index) =>( 
                     <div key={index} onClick={() => navigate(value.ruta)}>
                         <Carrusel nombre={value.name} imagen={value.img} descrip={value.descr}/>
                     </div>
                 ))}
-                
+
             </section>
 
             <footer className="mainDataFooter">
