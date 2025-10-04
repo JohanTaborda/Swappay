c8a1e9eba0de0b6cc60c34706f7308a6b5b51b3b
AsleyT <torrespalacioas@gmail.com>
2025-10-04 01:34:02 -0500

    Se creó un formulario para la publicación de productos, junto con un botón flotante que abre el formulario al hacer clic.

--- src/modules/layouts/main/MainPanel/MainPanel.jsx
+++ src/modules/layouts/main/MainPanel/MainPanel.jsx
@@ -1,7 +1,8 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom"; //Importamos useLocation para validar rutas.
 
 import MainHeader from "./components/MainHeader/MainHeader"; //Importamos el componente General que siempre se mostrara.
+import ProductForm from "../../../../modules/products/ProductForm/ProductForm";
 
 //Importamos los componentes que cambiaran según la ruta existente.
 import MainData from "./components/MainData/MainData";
@@ -9,6 +10,8 @@ import Profile from "../../../../pages/Profile/Profile.jsx";
 import Configuration from "../../../../pages/Configuration/Configuration.jsx";
 import Offers from "../../../../pages/Offers/Offers.jsx";
 import Exchanges from "../../../../pages/Exchanges/Exchanges.jsx";
+import { IoAdd } from "react-icons/io5";
+import "./MainPanel.css"
 
 const MainPanel = () =>{
     const location = useLocation(); //Usamos el hook de useLocation para ver la ubicación actual.
@@ -20,10 +23,15 @@ const MainPanel = () =>{
     else if (location.pathname === '/intercambios') contentSection = <Exchanges/>;
     else contentSection = <MainData/>;
 
+    const [showModal, setShowModal] = useState(false); //Estado para controlar el modal.
+
     return (
         <div>
             <MainHeader />
             {contentSection}
+        
+            <button className="buttonProductForm" onClick={() => setShowModal(true)} title="Abrir formulario"> <IoAdd /> </button> {/*Botón flotante para abrir el formulari. Al dar click showModal a true (abre el modal)*/}
+            <ProductForm open={showModal} onClose={() => setShowModal(false)} /> {/*Componente del formulario de producto dentro de un modal*/}
         </div>
     );
 }
